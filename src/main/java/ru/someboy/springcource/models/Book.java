a62c359f07c1b779231bdca4231d8f05ff68bc30
someboygit <someboygit@gmail.com>
2022-08-26 09:27:55 +0300

    Create project Library Add models Spring, Hibernate-validator,Thymeleaf, PostgreSQL are connected

---
+++ src/main/java/ru/someboy/springcource/models/Book.java
@@ -0,0 +1,62 @@
+package ru.someboy.springcource.models;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+/**
+ * @author Slipets Artem
+ */
+public class Book {
+    private int book_id;
+
+    @NotEmpty(message = "Title should not be empty")
+    @Size(min = 2, max = 30, message = "Title should be between 2 and 50 characters")
+    private String title;
+
+    @NotEmpty(message = "Author should not be empty")
+    @Size(min = 2, max = 30, message = "Author should be between 2 and 30 characters")
+    private String author;
+
+    @Min(value = 0, message = "year of birth should be greater than 0")
+    private int yearOfPublication;
+
+    public Book(int book_id, String title, String author, int yearOfPublication) {
+        this.book_id = book_id;
+        this.title = title;
+        this.author = author;
+        this.yearOfPublication = yearOfPublication;
+    }
+
+    public int getBook_id() {
+        return book_id;
+    }
+
+    public void setBook_id(int book_id) {
+        this.book_id = book_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYearOfPublication() {
+        return yearOfPublication;
+    }
+
+    public void setYearOfPublication(int yearOfPublication) {
+        this.yearOfPublication = yearOfPublication;
+    }
+}
---
+++ src/main/java/ru/someboy/springcource/models/Person.java
@@ -0,0 +1,49 @@
+package ru.someboy.springcource.models;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+/**
+ * @author Slipets Artem
+ */
+public class Person {
+    private int person_id;
+
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
+    private String name;
+
+    @Min(value = 1900, message = "year of birth should be greater than 1900")
+    private int yearOfBirth;
+
+    public Person(int id, String name, int year_of_birth) {
+        this.person_id = id;
+        this.name = name;
+        this.yearOfBirth = year_of_birth;
+    }
+
+    public int getPerson_id() {
+        return person_id;
+    }
+
+    public void setPerson_id(int person_id) {
+        this.person_id = person_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+}
